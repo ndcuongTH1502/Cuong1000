755261844e7778f2df1b1290a00d5d3d58a64315
unknown <cuong.nd0602@gmail.com>
2018-03-12 00:19:39 +0700

    first commit

---
+++ Client/Client/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Client.path = "C:/Users/nguye/Desktop/"; //mặc định nhận file tại desktop
+        }
+        public static string path;
+        public static string MessageCurrent = "Stopped";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(Client .path .Length >0)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Not Receive File");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = Client.MessageCurrent + Environment.NewLine + Client.path;
+        }
+        Client server = new Client();
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            server.StartServer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Client.path.Length > 0)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Not Receive File");
+            }
+        }
+    }
+}
---
+++ Server/Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+namespace Client
+{
+    class Server
+    {
+        public static string MessageCurrent = "Idle";
+        public static void SendFile(string fName)
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse("192.168.1.50");//IP lấy làm server
+                IPEndPoint end = new IPEndPoint(ip, 2014);
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                string path = "";
+                fName = fName.Replace("\\", "/");
+                while (fName.IndexOf("/") > -1)
+                {
+                    path += fName.Substring(0, fName.IndexOf("/") + 1);
+                    fName = fName.Substring(fName.IndexOf("/") + 1);
+                }
+                //Chuyển đổi
+                byte[] fNameByte = Encoding.ASCII.GetBytes(fName);
+
+               MessageCurrent  = "Buffering...";
+                byte[] fileData = File.ReadAllBytes(path + fName);
+                byte[] clientData = new byte[4 + fNameByte.Length + fileData.Length];
+                byte[] fNameLen = BitConverter.GetBytes(fNameByte.Length);
+                fNameLen.CopyTo(clientData, 0);
+                fNameByte.CopyTo(clientData, 4);
+                fileData.CopyTo(clientData, 4 + fNameByte.Length);
+                MessageCurrent = "Connect to Server....";
+                sock.Connect (end);
+                //Gửi
+                MessageCurrent = "The file is being sent....." ;
+                sock .Send(clientData );
+                //Đóng kn
+                sock.Close (); 
+                MessageCurrent ="The file was sent...";
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
